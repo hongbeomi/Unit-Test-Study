2d1beb2737ac711ab4f1a1693475a51e01ffa85a
hongbeomi <dks3963@gmail.com>
2022-10-09 15:45:44 +0900

    4장 좋은 단위 테스트의 4대 요소 - 거짓 양성 테스트 (소스 검사)

--- Sample/src/test/kotlin/chapter4/false_positive/MessageRendererTest.kt
+++ Sample/src/test/kotlin/chapter4/false_positive/MessageRendererTest.kt
@@ -1,6 +1,7 @@
 package chapter4.false_positive
 
 import org.junit.jupiter.api.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 internal class MessageRendererTest {
@@ -15,6 +16,29 @@ internal class MessageRendererTest {
         assert(renderers[0] is HeaderRenderer)
         assert(renderers[1] is BodyRenderer)
         assert(renderers[2] is FooterRenderer)
+    }
+
+    @Test
+    fun `MessageRenderer 소스 코드를_읽고_검사`() {
+        val sourceCode = File("./src/main/kotlin/chapter4/false_positive/MessageRenderer.kt")
+            .useLines { it.toMutableList() }
+            .drop(2)
+            .joinToString("\n")
+
+        assertEquals(
+            "class MessageRenderer : IRenderer {" +
+                    "\n\n" +
+                    "    val subRenderers: List<IRenderer> = listOf(\n" +
+                    "        HeaderRenderer(),\n" +
+                    "        BodyRenderer(),\n" +
+                    "        FooterRenderer(),\n" +
+                    "    )" +
+                    "\n\n" +
+                    "    override fun render(message: Message): String = subRenderers.joinToString { it.render(message) }" +
+                    "\n\n" +
+                    "}",
+            sourceCode
+        )
     }
 
 }
